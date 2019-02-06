0f42018a9c8bce0c244c0758f844655501346c54
Frederico Monteiro <frederico.dmonteiro@gmail.com>
2019-02-06 19:00:11 +0000

    orders

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using ECommerce.Classes;
+using ECommerce.Models;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
-using ECommerce.Classes;
-using ECommerce.Models;
 using static ECommerce.Classes.UserHelper;
 
 namespace ECommerce.Controllers
---
+++ Models/Orders.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Models
+{
+    public class Orders
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [Range(1, double.MaxValue, ErrorMessage = "You must select a {0}")]
+        [Display(Name = "Customer")]
+        public int CustomerId { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [Range(1, double.MaxValue, ErrorMessage = "You must select a {0}")]
+        [Display(Name = "State")]
+        public int StateId { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Remarks { get; set; }
+
+        public virtual Customer Customer { get; set; }
+
+        public virtual State State { get; set; }
+    }
+}
